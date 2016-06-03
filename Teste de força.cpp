a5aea00f609f017fb1bd31f2e7820de12dac05ef
victor-mesmo <vrodrigues153@gmail.com>
2016-06-03 19:20:38 -0300

    ATUALIZAOÇAO !!!
    no primeiro não consegui fazer o programa perfeitamente mais agr tá
    melhor

--- Teste de força.cpp
+++ Teste de força.cpp
@@ -12,9 +12,9 @@ int main(int argc, char* args[])
 {
 	// bool palavra-chave pode ter valores true e false.
 	bool sair = false;
-	
+
 	string jog1, jog2; // variavel 
-	
+
 	int vida1 = 20, vida2 = 20; // outra variavel de numeros inteiros, a variavel esta salvando o numero 20 das vida1 e vida2
 	// saida
 	cout << "Digite o nome do jogador 1:" << endl;
@@ -24,7 +24,7 @@ int main(int argc, char* args[])
 	cout << "Digite o nome do jogador 2:" << endl; // mesma coisa do primeiro 
 	cin >> jog2;
 	// while- laço e usado quando nao sabemos quantas vezes vamos repetir os laço ou quando vamos parar
-	
+
 	while (sair == false) // sair == false, retomando a bool para dizer quando queremos sair do laço
 	{
 		cout << " o         o" << endl;
@@ -35,9 +35,7 @@ int main(int argc, char* args[])
 
 		cout << "proxima jogada ? (Digite enter)" << endl;
 		cin.get();
-		cout << "proxima jogada ? (Digite enter)" << endl;
-		cin.get();
-		cin.get();
+		
 		system("pause");// vai deixar o programa pausado ate que alguem aperte alguma tecla
 
 		int dado6;
@@ -50,14 +48,14 @@ int main(int argc, char* args[])
 		vida2 = vida2 - dado6;
 
 		cout << "vida: " << vida1 << "vida: " << vida2 << endl;
-
-		system("cls"); // limpa a tela
+		system("pause");// vai deixar o programa pausado ate que alguem aperte alguma tecla
+		
 
 		if (vida1 <= 0 || vida2 <= 0) // se a vida for menor ou igual a 0 vai sair..
 		{
 			sair = true;
 		}
-		else system("cls"); // se nao limpa a tela 
+		else system("cls"); // se nao, limpa a tela 
 
 		cout << "FIM DE JOGO!" << endl;
 
@@ -66,6 +64,7 @@ int main(int argc, char* args[])
 		}
 		else if (vida1 > 0) cout << "jog2" << "venceu !" << endl; //se o jogado 2 tiver mais vida ele vence
 		else cout << "Os dois perderam!" << endl; // se nenhum dos dois vencerem vai dar empate 
+		system("pause");
 	}
 	return 0; // retorno e 0
 }
